65972fe35e79b1fb3b08a99ee71d5f7f888c1f19
Radosław Biesek <radoslaw.biesek@gmail.com>
2019-03-01 20:10:29 +0100

    Add action types, action creators, reducers, store, Provider and DevTools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import DevTools from './DevTools';
+import { getCountries } from './actions/actions-countries';
 
 ReactDOM.render(
-    <h1>Test</h1>, 
+    <Provider store={store}>
+        <div>
+            <h1>Test</h1>
+            <DevTools />
+        </div>
+    </Provider>,
     document.getElementById('root')
 );
+
+store.dispatch(getCountries());
